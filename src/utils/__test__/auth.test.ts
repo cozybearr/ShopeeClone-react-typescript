699c720e0b6f3a873d0d74429d4c97c81e25799c
Bùi Quốc Minh Quân <buiquocminhquan.work@gmail.com>
2024-07-10 21:19:50 +0700

    Fix profile

--- src/utils/__test__/auth.test.ts
+++ src/utils/__test__/auth.test.ts
@@ -8,9 +8,6 @@ const access_token =
 const refresh_token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ODBkZmZjYmI2NTk3MDMzNjYxMWI1MCIsImVtYWlsIjoicWJ1aUBnbWFpbC5jb20iLCJyb2xlcyI6WyJVc2VyIl0sImNyZWF0ZWRfYXQiOiIyMDI0LTA3LTA5VDEzOjE5OjQwLjQwNVoiLCJpYXQiOjE3MjA1MzExODAsImV4cCI6MTcyMDYxNzU4MH0.X42xpgWHzvquYn3qPtlg76ZizeSAETxUej9HLc8-_D0'
 
-const profile =
-  '{"_id":"6680dffcbb65970336611b50","roles":["User"],"email":"qbui@gmail.com","createdAt":"2024-06-30T04:33:00.646Z","updatedAt":"2024-07-08T06:13:24.862Z","__v":0,"date_of_birth":"2006-09-14T17:00:00.000Z","name":"Quan Bui","phone":"09068097301","avatar":"aca06b74-250c-4af1-a85f-140f0d99a978.png"}'
-
 beforeEach(() => {
   localStorage.clear
 })
